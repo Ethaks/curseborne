351721a2f14efd66d6790acea1b9cc17e73d4c2c
Ethaks <ethaks@protonmail.ch>
2025-11-09 15:43:29 +0100

    feat: add play/edit mode to TextEditorApplication

--- module/applications/dialogs/text-editor.mjs
+++ module/applications/dialogs/text-editor.mjs
@@ -1,9 +1,12 @@
+import { CurseborneDocumentSheetMixin } from "@applications/sheets/document-mixin.mjs";
 import { SYSTEM_ID } from "@helpers/utils.mjs";
 
 /**
  * A simple application serving to edit a given document's `HTMLField` property.
  */
-export class TextEditorApplication extends foundry.applications.api.DocumentSheetV2 {
+export class TextEditorApplication extends CurseborneDocumentSheetMixin(
+	foundry.applications.api.DocumentSheetV2,
+) {
 	/** @inheritDoc */
 	static DEFAULT_OPTIONS = {
 		sheetConfig: false,
@@ -34,20 +37,32 @@ export class TextEditorApplication extends foundry.applications.api.DocumentShee
 		const context = await super._prepareContext(options);
 		context.field = this.options.field;
 		context.fieldPath = this.options.fieldPath ?? this.field.fieldPath;
-		context.value = foundry.utils.getProperty(this.document, context.fieldPath);
+		const value = foundry.utils.getProperty(this.document, context.fieldPath);
+		context.value = this.isPlayMode
+			? await foundry.applications.ux.TextEditor.enrichHTML(value, {
+					relativeTo: this.document,
+					rollData: context.rollData ?? this.document.getRollData(),
+					secrets: this.document.isOwner,
+				})
+			: value;
 		return context;
 	}
 
 	/** @inheritDoc */
 	async _renderHTML(context, _options) {
-		const input = this.options.field._toInput({
+		// Simply display enriched HTML in play mode
+		if (this.isPlayMode) {
+			return foundry.utils.parseHTML(`<div class="enriched scrollable">${context.value}</div>`);
+		}
+
+		// Let HTMLField create the prose mirror input
+		return this.options.field._toInput({
 			name: context.fieldPath,
 			value: context.value,
 			toggled: false,
 			documentUUID: this.document.uuid,
 			height: 600,
 		});
-		return input;
 	}
 
 	/** @inheritDoc */
